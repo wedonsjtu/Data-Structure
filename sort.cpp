a5b8b89438a1d5173de5748656757e8e77681d28
wedonsjtu <wedonsjtu@sjtu.edu.cn>
2018-11-30 15:00:44 +0800

    Add some sort functions

--- sort.cpp
+++ sort.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 template <class T>
-void simpleInsertSort(T a[], int size) {
+void insertSort(T a[], int size) {
 	int j, k;
 	T tmp;
 	for (j = 1; j < size; ++j) {
@@ -30,7 +30,7 @@ void shellSort(T a[], int size) {
 //=========================================================
 
 template <class T>
-void simpleSelectSort(T a[], int size) {
+void selectSort(T a[], int size) {
 	int i, j, minindex;
 	T tmp;
 	for (i=0; i < size - 1; ++i) {
@@ -95,11 +95,74 @@ void bubbleSort(T a[], int size) {
 
 //=========================================================
 
+template <class T>
+int divide(T a[], int low, int high) {
+	T k = a[low];
+	do {
+		while (low < high && a[high] >= k) --high;
+		if (low < high) a[low++] = a[high];
+		while (low < high && a[low] <= k) ++low;
+		if (low < high) a[high--] = a[low];
+	}
+	while (low != high);
+	a[low] = k;
+	return low;
+
+}
+
+template <class T>
+void qSort(T a[], int low, int high) {
+	int mid;
+	if (low >= high) return;
+	mid = divide(a, low, high);
+	qSort(a, low, mid-1);
+	qSort(a, mid+1, high);
+}
+
+template <class T>
+void quickSort(T a[], int size) {
+	qSort(a, 0, size-1);
+}
+
+//=========================================================
+
+template <class T>
+void merge(T a[], int left, int mid, int right) {
+	T *tmp = new T[right-left+1];
+	int i = left, j = mid, k = 0;
+	while (i < mid && j <= right) {
+		if (a[i] <= a[j]) tmp[k++] = a[i++];
+		else tmp[k++] = a[j++];
+	}
+	while (i < mid) tmp[k++] = a[i++];
+	while (j <= right) tmp[k++] = a[j++];
+	for (i=0, k=left; k <= right; k++, i++) a[k] = tmp[i];
+	delete[] tmp;
+}
+
+template <class T>
+void mSort(T a[], int left, int right) {
+	int mid = (left + right) / 2;
+	if (left == right) return;
+	mSort(a, left, mid);
+	mSort(a, mid+1, right);
+	merge(a, left, mid+1, right);
+}
+
+template <class T>
+void mergeSort(T a[], int size) {
+	mSort(a, 0, size-1);
+}
+
+//=========================================================
+
+/*
 int main() {
 	int s[] = {7,4,2,4,8,2,74,21,43,21,65,34,23};
-	bubbleSort(s, sizeof(s) / sizeof(s[0]));
+	mergeSort(s, sizeof(s) / sizeof(s[0]));
 	for (int i = 0; i < sizeof(s) / sizeof(s[0]); ++i) {
 		cout << s[i] << ' ';
 	}
 	cout << endl;
 }
+*/
